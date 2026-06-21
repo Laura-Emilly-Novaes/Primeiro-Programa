3564b07e9e5dbbe8424459b4eeb31052dcacbc9f
Laura Emilly Novaes <lauraemillynovaes@gmail.com>
2026-06-21 12:54:12 -0300

    Delete praticando.py

--- praticando.py
+++
@@ -1,7 +0,0 @@
-print ("\n\n===============\n")
-print ("Programa de Tabuada \n") #vai aparecer na tela
-num = int ( input ("Digite qual tabuada você quer: ")) #recebe o multiplicando
-for i in range (11): #ele faz a multiplicação daquela tabuada
-        print (num, "x", i, "=", num*i) #organização de aparecer na tela
-print (" ")
-print ("===============\n")
